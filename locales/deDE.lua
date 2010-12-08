45b81f16ef2ad317dbe8c275cbcb75cf5b5deeb9
Adam Hellberg <adam.hellberg95@gmail.com>
2010-12-08 21:03:02 +0000

    Fixed: German locale should not load by default anymore.
    git-svn-id: svn://svn.curseforge.net/wow/chocobo/mainline/trunk@33 b5868af5-df1d-4ace-9c31-b94f5fcd69ed

--- locales/deDE.lua
+++ locales/deDE.lua
@@ -1,7 +1,7 @@
---if (GetLocale() == "deDE") then
+if (GetLocale() == "deDE") then
 	local L = _G["ChocoboLocale"]
 	
 	--@localization(locale="deDE", format="lua_additive_table", handle-unlocalized="english")@
 	
 	setmetatable(L, {__index=ChocoboLocale_Default})
---end
+end
